d7b3b38358df277d7b790c79a366ebbf78aaaefb
eronfelix-js <eronf5594@gmail.com>
2025-05-08 14:13:20 -0300

    first commit

---
+++ src/main/java/com/Devfelix/CadastroDeNinja/CadastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.Devfelix.CadastroDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjaApplication.class, args);
+	}
+
+}
